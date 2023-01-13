2672ac377e4be3e0f180a815646d1abc47c311fd
JanluPaulsen <jpaulsen1317@gmail.com>
2023-01-13 20:59:47 +0200

    Comments

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        //counter to track the amount of prime numbers found
         int primeNumCounter = 0;
+        //for loop to keep going till i = 1000
         for (int i = 0; i <= 1000; i++){
             isPrime(i);
+            //call the method
             if (isPrime(i)) {
                 primeNumCounter++;
                 System.out.println(i + " is a prime number");
                 System.out.println(primeNumCounter + " prime numbers found");
+                //when the counter reaches 3 break the for loop
                 if (primeNumCounter == 3) {
                     break;
                 }
@@ -15,9 +19,11 @@ public class Main {
     }
 
     public static boolean isPrime(int wholeNumber) {
+        //0 and 1 are not prime number so start at 2
         if (wholeNumber <= 2) {
             return (wholeNumber == 2);
         }
+        //if it can divide by 2 and other values other than just itself it is not a prime number
         for (int divider = 2; divider <= wholeNumber / 2; divider++) {
             if (wholeNumber % divider == 0) {
                 return false;
